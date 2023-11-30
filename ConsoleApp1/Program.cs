d982f6e1232afc314a708485a278fc2526908c41
Oruco_ac26 <Oruco_ac26@STHQ0122-08>
2023-11-30 20:10:26 +0400

    Dialog

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,97 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Dialog
+{
+
+    public interface IButton
+    {
+        void render();
+        void OnClick(Action click);
+    }
+
+    public class WindowsButton : IButton
+    {
+        public void OnClick(Action click)
+        {
+            //OnClick
+        }
+
+        public void render() { }
+    }
+
+    public class HtmlButton : IButton
+    {
+        public void OnClick(Action click)
+        {
+            //OnCLick
+        }
+
+        public void render() { }
+    }
+
+    public abstract class Dialog
+    {
+        public void render()
+        {
+            IButton okButton = CreateButton();
+            okButton.OnClick(CloseDialog);
+            okButton.render();
+        }
+
+        private void CloseDialog()
+        {
+            //CLoseDialog
+        }
+
+        public abstract IButton CreateButton();
+    }
+
+    public class WindowsDialog : Dialog
+    {
+        public override IButton CreateButton()
+        {
+            return new WindowsButton();
+        }
+    }
+
+    public class WebDialog : Dialog
+    {
+        public override IButton CreateButton()
+        {
+            return new HtmlButton();
+        }
+    }
+
+    public class Creator
+    {
+        public Dialog CreateDialog(string platform)
+        {
+            Dialog dialog;
+
+            switch (platform.ToLower())
+            {
+                case "windows":
+                    dialog = new WindowsDialog();
+                    break;
+                case "web":
+                    dialog = new WebDialog();
+                    break;
+                default:
+                    throw new ArgumentException("Invalid platform specified");
+            }
+
+            return dialog;
+        }
+    }
+
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Creator dialogCreator = new Creator();
+            Dialog windowsDialog = dialogCreator.CreateDialog("windows");
+            windowsDialog.render();
+            Dialog webDialog = dialogCreator.CreateDialog("web");
+            webDialog.render();
+        }
+    }
+}
